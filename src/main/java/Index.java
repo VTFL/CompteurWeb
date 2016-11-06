db25cfc2353bc3c993b84cad797802431199864d
LucasPatard <patard.lucas@gmail.com>
2016-11-06 17:58:08 +0100

    cookie :3

--- src/main/java/Index.java
+++ src/main/java/Index.java
@@ -5,15 +5,31 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 import java.io.IOException;
 
 public class Index extends HttpServlet {
 
+	public static int userID =0;
 	@Override
 	public void doGet( HttpServletRequest request, HttpServletResponse response )
 			throws ServletException, IOException {
 		String[] listpays = {"France","Royaume-Uni","USA","Russie","Yemen"};
 		request.setAttribute("listpays",listpays);
+		Cookie[] cookies = request.getCookies();
+		boolean cookieExist = false;
+		for(Cookie cookie : cookies){
+			if(cookie.getName() == "userID"){
+				cookieExist = true;
+			}
+		}
+		if(!cookieExist){
+			response.addCookie(new Cookie("userID",""+userID));
+			++userID;
+		}
 		this.getServletContext()
 				.getRequestDispatcher( "/index.jsp" )
 				.forward( request, response );
