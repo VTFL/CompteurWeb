f5754ef02ffb03ad9605b274d69c98e9dd60cd95
LucasPatard <patard.lucas@gmail.com>
2016-11-06 18:25:44 +0100

    cookie :3

--- src/main/java/websocket/CreateTimer.java
+++ src/main/java/websocket/CreateTimer.java
@@ -5,7 +5,7 @@ package websocket;
  */
 
 import java.io.IOException;
-
+import java.util.HashMap;
 
 import org.json.*;
 import javax.websocket.OnMessage;
@@ -17,16 +17,17 @@ import javax.websocket.server.ServerEndpoint;
 public class CreateTimer {
 
     // A chaque fois qu'on reçoit un nouveau message de la part du client on cré un nouveau Timer
-    // on stock le Timer
+    // on stock le Timer dans la base avec sont userID
     // et on met en place la boucle qui envoi au client le timer mis a jour toute les secondes
-    private static HashMap<Integer,>
+
     @OnMessage
     public void onMessage(Session session, String jsonMessage) {
         try {
             if (session.isOpen()) {
                 JSONObject data = new JSONObject(jsonMessage);
+
 				session.getBasicRemote().sendText(data.getString("langue"));
-                session.getId()
+                session.getId();
             }
         } catch (Exception e) {
             try {
@@ -36,6 +37,7 @@ public class CreateTimer {
             }
         }
     }
+
 
 
 }
