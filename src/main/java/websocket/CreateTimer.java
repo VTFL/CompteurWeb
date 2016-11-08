5f48322fbf4a5c9ff6a2c5fa5a0362b9be404f61
valentin pitel <valentin.pitel@live.fr>
2016-11-08 17:46:43 +0100

    Compteur.java:    =>ajout d'une variable
    CreateTimer.java :
       =>test d'ajout a la bdd sur la reception d'un message sur le serveur
       =>récupération des compteurs de la bdd
       =>envoie des infos de tous les compteur via json
    
     index.jsp:
       =>modification de la méthode onmessage (client websocket)
    
     pom.xml:
       =>ajout de dépendances (Gson, transformation d'un objet en json)

--- src/main/java/websocket/CreateTimer.java
+++ src/main/java/websocket/CreateTimer.java
@@ -4,6 +4,9 @@ package websocket;
  * Created by valentinpitel on 04/11/2016.
  */
 
+import beans.Compteur;
+import com.google.gson.Gson;
+import db.DatabaseManager;
 import org.json.JSONObject;
 
 import javax.websocket.OnMessage;
@@ -11,9 +14,7 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -24,6 +25,7 @@ public class CreateTimer {
     static ScheduledExecutorService timer =
             Executors.newSingleThreadScheduledExecutor();
     private static Set<Session> allSessions;
+    private DatabaseManager dbm = null;
     // A chaque fois qu'on reçoit un nouveau message de la part du client on cré un nouveau Timer
     // on stock le Timer dans la base avec sont userID
     // et on met en place la boucle qui envoi au client les timers mis a jour toutes les secondes
@@ -33,7 +35,7 @@ public class CreateTimer {
         try {
             if (session.isOpen()) {
                 JSONObject data = new JSONObject(jsonMessage);
-
+                dbm.ajouterCompteur(new Compteur(23,"Gestion de projet",1,"10/11/2016 08:00:00",0));
 				session.getBasicRemote().sendText(data.getString("userID"));
                 session.getId();
             }
@@ -45,44 +47,33 @@ public class CreateTimer {
             }
         }
     }
+    //faire en fonction des cookies
     @OnOpen
     public void showTimer(Session session){
+        dbm= new DatabaseManager();
         // on envoie toute les secondes
         timer.scheduleAtFixedRate(
                 () -> sendTimeToAll(session),0,1, TimeUnit.SECONDS);
     }
+
     private void sendTimeToAll(Session session){
-        allSessions = session.getOpenSessions();
-        for (Session sess: allSessions){
-            try{
-                sess.getBasicRemote().sendText("temps restant :"+diff());
-            } catch (IOException ioe) {
-                System.out.println(ioe.getMessage());
+       // allSessions = session.getOpenSessions();
+        ArrayList<Compteur> lst_compteur = new ArrayList<>();
+      //  for (Session sess: allSessions){
+        try{
+            Gson gson = new Gson();
+            lst_compteur=dbm.getCompteurs(Integer.parseInt(session.getId()));
+            Compteur c ;
+            for(int i = 0; i<lst_compteur.size();i++){
+                c=lst_compteur.get(i);
+                c.setMajCompteur(c.diff());
             }
-        }
-    }
-    // on changera pour mettre une date non fixe
-    private String diff(){
-        String theDate = "08/11/2016 17:30:00";
-        String pattern = "dd/MM/yyyy HH:mm:ss";
-        Date d2 = null;
-        try {
-            d2 = new SimpleDateFormat(pattern).parse(theDate);
-        } catch (ParseException e) {
-            return "server error...";
-        }
-        Date d1 = new Date();
+            String json = gson.toJson(lst_compteur);
+            session.getBasicRemote().sendText(json);
 
-        long diff = d2.getTime() - d1.getTime();
-
-        long diffSeconds = diff / 1000 % 60;
-        long diffMinutes = diff / (60 * 1000) % 60;
-        long diffHours = diff / (60 * 60 * 1000) % 24;
-        long diffDays = diff / (24 * 60 * 60 * 1000);
-        return diffDays+" jour(s) "+diffHours+" heure(s) "+diffMinutes+" minute(s) "+diffSeconds+" seconde(s)";
+        } catch (IOException ioe) {
+            System.out.println(ioe.getMessage());
+        }
 
     }
-
-
-
 }
