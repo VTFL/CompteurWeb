af543fe221e9485fdd4ad805654a9f2eabca179a
Dropth <florian.alline@etu.univ-lehavre.fr>
2016-11-18 23:43:12 +0100

    Ajout : Permet à l'utilisateur de créer un compteur aléatoire dont la date est automatiquement après la date actuelle et maximum 100 ans après afin de plus facilement tester le bon fonctionnement des compteurs.

--- src/main/java/websocket/CreateTimer.java
+++ src/main/java/websocket/CreateTimer.java
@@ -28,7 +28,7 @@ public class CreateTimer {
             Executors.newSingleThreadScheduledExecutor();
     private static Set<Session> allSessions;
     private DatabaseManager dbm = null;
-    // A chaque fois qu'on reçoit un nouveau message de la part du client on cré un nouveau Timer
+    // A chaque fois qu'on reçoit un nouveau message de la part du client on crée un nouveau Timer
     // on stock le Timer dans la base avec sont userID
     // et on met en place la boucle qui envoi au client les timers mis a jour toutes les secondes
 
